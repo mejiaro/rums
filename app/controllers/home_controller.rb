6bce644eba5ad730835b4af107d8367f9a3ed33b
Ricardo Mejía <mejiaro@gmail.com>
2018-10-08 23:28:44 -0600

    remove location selector, show all reservations in calendar, and show the location where the reservation was made instead

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    @reservations = current_user.reservations.where(room_id: params[:room_id])
+    @reservations = current_user.reservations
   end
 end

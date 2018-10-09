3c369dee3e171270d39c64e12002910287dc95d2
Ricardo Mejía <mejiaro@gmail.com>
2018-10-08 18:08:19 -0600

    siwth room to see reservations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-    @reservations = current_user.reservations
+    @reservations = current_user.reservations.where(room_id: params[:room_id])
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,7 +7,6 @@ class Reservation < ApplicationRecord
 
   def confirmed_reservations_must_be_less_or_equal_to_max_reservations
     errors.add(:room, "must be less or equal to max reservations") if
-      #room.reservations.where("").count + 1 > room.max_reservations
-      room.reservations.where(start_time: self.start_time.beginning_of_day..self.start_time.end_of_day).count+1  > room.max_reservations
+    room.reservations.where(start_time: self.start_time.beginning_of_day..self.start_time.end_of_day).count+1  > room.max_reservations
   end
 end

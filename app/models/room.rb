44f9b635ba9c00143c1b660cedd2f564e4a1e374
Ricardo Mejía <mejiaro@gmail.com>
2018-10-09 17:43:49 -0600

    show # of reservations of room, add prettier day/month format on calendar

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,7 @@
 class Room < ApplicationRecord
   has_many :reservations
+
+  def total_reservations(date_start, date_end)
+    reservations.where(start_time: date_start..date_end).count
+  end
 end
